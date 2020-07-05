8f7257e1422446d239e92c2c3844bf431bef27f8
Haven <haventh@163.com>
2020-07-05 21:25:29 +0800

    test3

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,5 +6,7 @@
 public class Application {
     public static void main(String[] args) {
         System.out.println("test2");
+        System.out.println("原本");
+        System.out.println("追加");
     }
 }
