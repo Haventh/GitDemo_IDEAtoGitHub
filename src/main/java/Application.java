d68c9dabfdff455b179127cf2cc99aa3a79c5d46
Haven <haventh@163.com>
2020-07-05 20:42:58 +0800

    test1

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,4 +4,7 @@
  * @Description:
  */
 public class Application {
+    public static void main(String[] args) {
+
+    }
 }
---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: GHZ
+ * @Date: 2020/7/5 20:36
+ * @Description:
+ */
+public class Hello {
+}
