742c857d9bb9edc4d56d96297fcd34477393ab74
Haven <haventh@163.com>
2020-07-05 20:35:08 +0800

    test

---
+++ src/main/java/Application.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: GHZ
+ * @Date: 2020/7/5 20:32
+ * @Description:
+ */
+public class Application {
+}
---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+/**
+ * @Author: GHZ
+ * @Date: 2020/7/5 20:33
+ * @Description:
+ */
+public class Test {
+}
