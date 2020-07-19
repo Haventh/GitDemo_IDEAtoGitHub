444cbad5d8dcdb8bff806df341c5c6a18709e1d9
Haventh <35954683+Haventh@users.noreply.github.com>
2020-07-19 23:17:01 +0800

    Update Application.java
    在GitHub添加的的的的的

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -12,5 +12,6 @@ public class Application {
         System.out.println("在GitHub添加的的");
         System.out.println("在GitHub添加的的的");
         System.out.println("在GitHub添加的的的的");
+        System.out.println("在GitHub添加的的的的的");
     }
 }
